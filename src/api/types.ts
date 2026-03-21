3b8d1a9a1121ba4c302bf040b4c63dadebff7b06
Alexey <eaa04@bk.ru>
2026-03-21 23:13:36 +0300

    add: films list page

--- src/api/types.ts
+++ src/api/types.ts
@@ -9,16 +9,26 @@ export interface Rating {
 
 export interface Poster {
   url?: string | null;
+  previewUrl?: string | null;
 }
 
 export interface Movie {
   id: number;
   name?: string | null;
+  alternativeName?: string | null;
   year?: number | null;
   rating?: Rating | null;
   genres?: Genre[] | null;
   description?: string | null;
   poster?: Poster | null;
+}
+
+export interface SearchMoviesResponse {
+  docs: Movie[];
+  total: number;
+  limit: number;
+  page: number;
+  pages: number;
 }
 
 export interface ApiError {
